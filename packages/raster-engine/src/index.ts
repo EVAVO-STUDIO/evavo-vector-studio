52233b0f3ab20467bb543593408115ce1a5011f5
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-28 14:25:43 +1000

    feat(raster): expose tracing and inspection API

---
+++ packages/raster-engine/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./analysis.js";
+export * from "./engine.js";
+export * from "./errors.js";
+export * from "./preflight.js";
+export * from "./presets.js";
+export * from "./types.js";
