739afd28d58a7ef1c4ac3ad2c7121b66166ce42a
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-28 20:33:56 +1000

    fix(lottie): publish unambiguous package exports

--- packages/lottie-engine/src/index.ts
+++ packages/lottie-engine/src/index.ts
@@ -2,5 +2,12 @@ export * from "./errors.js";
 export * from "./generator.js";
 export * from "./inspection.js";
 export * from "./path-data.js";
-export * from "./svg-source.js";
 export * from "./types.js";
+export { prepareSvgSourceForLottie } from "./svg-source.js";
+export type {
+  ExtractedSvgPath,
+  LottiePathStyle,
+  LottieSolidPaint,
+  PreparedLottieSvgSource,
+  SvgRenderUnit,
+} from "./svg-source.js";
