bd1d047837fd748e547b5120984e0bb3ffdd9e9f
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-28 20:25:20 +1000

    feat(lottie): publish engine entrypoint

---
+++ packages/lottie-engine/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./errors.js";
+export * from "./generator.js";
+export * from "./inspection.js";
+export * from "./path-data.js";
+export * from "./svg-source.js";
+export * from "./types.js";
