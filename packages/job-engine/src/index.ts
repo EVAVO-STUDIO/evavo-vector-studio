9d18f2243931d8e342de912e966718904bd46c04
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-29 02:12:09 +1000

    build(jobs): export canonical path policy

--- packages/job-engine/src/index.ts
+++ packages/job-engine/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./errors.js";
 export * from "./inspection.js";
 export * from "./manifest.js";
+export * from "./path-policy.js";
 export * from "./runner.js";
 export * from "./store.js";
 export * from "./types.js";
