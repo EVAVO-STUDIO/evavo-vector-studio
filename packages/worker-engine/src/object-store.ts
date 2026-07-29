2916f63580b7f462493cf205fccf80cced9a9e82
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-07-29 17:07:01 +1000

    feat(object-transfer): expose dependency-light object-store facade

---
+++ packages/worker-engine/src/object-store.ts
@@ -0,0 +1,13 @@
+export * from "./base-errors.js";
+export * from "./file-object-store.js";
+export * from "./memory-object-store.js";
+export type {
+  ObjectReceipt,
+  ObjectWrite,
+  StoredObject,
+  VectorObjectStore,
+} from "./types.js";
+export {
+  VECTOR_WORKER_MAX_OUTPUT_BYTES,
+  VECTOR_WORKER_MAX_SOURCE_BYTES,
+} from "./types.js";
