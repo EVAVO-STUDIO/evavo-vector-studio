e49ba1f7e16ab19dcb0918aeb4cb5af8af87b38f
EVAVO-STUDIO <evavo.studio@gmail.com>
2026-08-31 14:56:25 +1000

    feat(raster): export region comparison evidence

--- packages/raster-engine/src/index.ts
+++ packages/raster-engine/src/index.ts
@@ -6,6 +6,7 @@ export * from "./errors.js";
 export * from "./input-policy.js";
 export * from "./preflight.js";
 export * from "./presets.js";
+export * from "./region-comparison.js";
 export * from "./runtime-guard.js";
 export * from "./selection.js";
 export * from "./types.js";
